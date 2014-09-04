7a8059b2295213572771e12e503254742109ad12
ogawayuta <Og123017@gmail.com>
2014-09-04 16:20:12 +0900

    sessionを使ったログインを可能にした

---
+++ orca_api.rb
@@ -0,0 +1,142 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+
+require 'uri'
+require 'net/http'
+require 'pp'
+require 'crack' # for xml and json
+require 'crack/xml' # for just xml
+Net::HTTP.version_1_2
+
+def post(url,opt,body)
+  req = Net::HTTP::Post.new(url)
+  req.content_length = body.size
+  req.content_type = "application/xml"
+  req.body = body
+  req.basic_auth(opt[:user],opt[:passwd])
+  
+  ret = []
+  Net::HTTP.start(opt[:host],opt[:port]) {|http|
+    res = http.request(req)
+    ret = [res.code,res.body]
+  }
+  ret
+end
+
+def list_patients(opt)
+  url = "/api01rv2/patientlst1v2?class=01"
+  body = <<-EOF
+    <data>
+      <patientlst1req type="record">
+        <Base_StartDate type="string">2000-01-01</Base_StartDate>
+        <Base_EndDate type="string">#{Time.now.strftime("%Y-%m-%d")}</Base_EndDate>
+        <Contain_TestPatient_Flag type="string">1</Contain_TestPatient_Flag>
+      </patientlst1req>
+    </data>
+  EOF
+
+  ret = post(url,opt,body)
+  if ret.empty?
+    puts "ret empty"
+    return []
+  end
+  unless ret[0] == "200"
+    puts "state code:#{ret[0]}"
+    return []
+  end
+
+  root = Crack::XML.parse(ret[1])
+  result = root["xmlio2"]["patientlst1res"]["Api_Result"]
+  unless result == "00"
+    puts "eroor"
+    return []
+  end
+  root["xmlio2"]["patientlst1res"]["Patient_Information"]
+end
+
+def register_patient(opt,params)
+  url = "/orca12/patientmodv2?class=01"
+  body = <<-EOF
+  <data>
+  	<patientmodreq type="record">
+  		<Patient_ID type="string">*</Patient_ID>
+  		<WholeName type="string">#{params['whole_name']}</WholeName>
+  		<WholeName_inKana type="string">#{params['whole_name_kana']}</WholeName_inKana>
+  		<BirthDate type="string">#{params['birth_date']}</BirthDate>
+  		<Sex type="string">#{params['sex']}</Sex>
+  		<HouseHolder_WholeName type="string">#{params['whole_name']}</HouseHolder_WholeName>
+  		<Relationship type="string">本人</Relationship>
+  		<Occupation type="string">会社員</Occupation>
+  		<CellularNumber type="string">09011112222</CellularNumber>
+  		<FaxNumber type="string">03-0011-2233</FaxNumber>
+  		<EmailAddress type="string">test@tt.dot.jp</EmailAddress>
+  		<Home_Address_Information type="record">
+  			<Address_ZipCode type="string">1130021</Address_ZipCode>
+  			<WholeAddress1 type="string">東京都文京区本駒込</WholeAddress1>
+  			<WholeAddress2 type="string">６−１６−３</WholeAddress2>
+  			<PhoneNumber1 type="string">03-3333-2222</PhoneNumber1>
+  			<PhoneNumber2 type="string">03-3333-1133</PhoneNumber2>
+  		</Home_Address_Information>
+  	</patientmodreq>
+  </data>
+  EOF
+
+  ret = post(url,opt,body)
+  if ret.empty?
+    puts "ret empty"
+    return [nil,"http post error"]
+  end
+  unless ret[0] == "200"
+    puts "state code:#{ret[0]}"
+    return [nil,"state code:#{ret[0]}"]
+  end
+
+  root = Crack::XML.parse(ret[1])
+  result = root["xmlio2"]["patientmodres"]["Api_Result"]
+  message = root["xmlio2"]["patientmodres"]["Api_Result_Message"]
+  unless result == "00"
+    puts "eroor"
+    return [nil,"result:#{result} message:#{message}"]
+  end
+
+  pinfo = root["xmlio2"]["patientmodres"]["Patient_Information"]
+  id = pinfo["Patient_ID"]
+  [id,nil]
+end
+
+def delete_patient(opt,params)
+  url = "/orca12/patientmodv2?class=03"
+  body = <<-EOF
+  <data>
+  	<patientmodreq type="record">
+  		<Patient_ID type="string">#{params['id']}</Patient_ID>
+  		<WholeName type="string">#{params['whole_name']}</WholeName>
+  		<WholeName_inKana type="string">#{params['whole_name_kana']}</WholeName_inKana>
+  		<BirthDate type="string">#{params['birth_date']}</BirthDate>
+  		<Sex type="string">#{params['sex']}</Sex>
+  	</patientmodreq>
+  </data>
+  EOF
+
+  ret = post(url,opt,body)
+  if ret.empty?
+    puts "ret empty"
+    return [nil,"http post error"]
+  end
+  unless ret[0] == "200"
+    puts "state code:#{ret[0]}"
+    return [nil,"state code:#{ret[0]}"]
+  end
+
+  root = Crack::XML.parse(ret[1])
+  result = root["xmlio2"]["patientmodres"]["Api_Result"]
+  message = root["xmlio2"]["patientmodres"]["Api_Result_Message"]
+  unless result == "00"
+    puts "eroor"
+    return [nil,"result:#{result} message:#{message}"]
+  end
+
+  pinfo = root["xmlio2"]["patientmodres"]["Patient_Information"]
+  id = pinfo["Patient_ID"]
+  [id,"削除しました"]
+end
---
+++ sinatra_oraca.rb
@@ -0,0 +1,92 @@
+#!/usr/bin/ruby
+#-*-coding: utf-8-*-
+$:.unshift File.dirname(__FILE__)
+
+require 'pp'
+require 'uri'
+require 'net/http'
+require 'crack'
+require 'crack/xml'
+require 'sinatra'
+require 'haml'
+require 'orca_api'
+
+enable :sessions
+
+set :bind, '0.0.0.0'
+set :public_folder, File.dirname(__FILE__) + '/static'
+
+Net::HTTP.version_1_2
+
+opt = {
+  :host => "192.168.4.123",
+  :port=> "8000",
+  :user => "ormaster",
+  :passwd => "ormaster123"
+}
+
+helpers do
+  def check_login
+    unless session[:login]
+      redirect to '/login'
+    end
+  end
+end
+
+get '/login' do
+  haml :login
+end
+
+post '/login' do
+  if params['user'] == 'soft' && params['passwd'] == 'bank'
+    session['login'] = true
+    redirect to '/'
+  else
+    session['message'] = 'ユーザまたはパスワードが違います'
+    session['login'] = nil
+    redirect to '/login'
+  end
+end
+
+get '/logout' do
+  session['login'] = nil
+  redirect to '/login'
+  haml :login
+end
+get '/' do
+  check_login
+  @patients = list_patients(opt)
+  haml :index
+end
+
+get '/register' do
+  check_login
+  haml :register
+end
+
+post '/register' do
+  check_login
+  @patient = params 
+  @id,@error = register_patient(opt,@patient)
+  if @error
+    session['message'] = @error
+    haml :register
+  else
+    haml :register_result
+  end
+end
+
+get '/delete' do
+  check_login
+  pp params
+  @patient = params
+  haml :delete
+end
+
+post '/delete' do
+  check_login
+  @params = params 
+  @id,@error = delete_patient(opt,@params)
+  session['message'] = @error
+  redirect to '/'
+end
